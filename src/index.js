5583e16a08a98cf17e29b5123cd280de93025083
Usman Rajput <bcawala3@gmail.com>
2023-10-28 00:08:34 +0530

    adding class in test.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>hello</h1>);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+class A extends React.Component {
+  // 2
 
+  constructor() {
+    super();
+    this.state = { name: "us", surname: "Rajput", course: "BCA" };
+  }
+  // method
+  render() {
+    return (
+      <>
+        <h1>
+          {this.props.name} {this.props.surname} from {this.props.children}
+        </h1>
+        <h1>
+          {this.state.name} {this.state.surname}
+          {this.state.course}
+        </h1>
+      </>
+    );
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <A name="anil" surname="doller">
+    {" "}
+    react
+  </A>
+);
